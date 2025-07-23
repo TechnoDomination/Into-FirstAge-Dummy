e03f73a4b6e1712246684c2bb4ba04df3f0a3c67
Dummy-Avika <avikalakhotia2012@gmail.com>
2025-07-23 19:02:01 -0400

    edited driveTrain/teleop/auto code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveTeleOp.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.firstinspires.ftc.teamcode.DriveTrain;
+@TeleOp(name = "TeleOp", group = "TeleOp")
 public class TankDriveTeleOp extends LinearOpMode {
 
     double frontLeftPower;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TimeBasedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TimeBasedAuto.java
@@ -1,11 +1,37 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.DriveTrain;
+@Autonomous(name = "Auto", group = "Auto")
 public class TimeBasedAuto extends LinearOpMode {
-
+int step=0;
     @Override
     public void runOpMode() throws InterruptedException {
         DriveTrain driveTrain = new DriveTrain(hardwareMap);
+        waitForStart();
+        while (opModeIsActive() && !isStopRequested()) {
+            switch (step) {
+                case 0:
+                    if (driveTrain.forward(1.0)) {  // move forward 1 second, returns true when done
+                        step++;
+                    }
+                    break;
+
+                case 1:
+                    if (driveTrain.rotate(1.0)) { // rotate left 1 second
+                        step++;
+                    }
+                    break;
+
+                case 2:
+                    if (driveTrain.backward(1.0)) {
+                        step++;
+                    }
+                    break;
+            }
+
+        }
     }
 }
